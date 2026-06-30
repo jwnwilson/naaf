2b48d22f356ba59c64ac9fe9c5e72eb2bff41959
Noel Wilson <jwnwilson@hotmail.co.uk>
2026-06-30 23:22:43 +0100

    chore: remove redundant root conftest.py (editable install handles imports)

--- conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-# Add src directories to path for pytest
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "projects/server/src"))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "libs/crud_router/src"))
