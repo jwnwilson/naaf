459251534ad4c1b250b4c72b34b6e40e8624532e
Noel Wilson <jwnwilson@hotmail.co.uk>
2026-07-02 10:30:20 +0100

    feat: add /threads read + send API

--- projects/server/src/interactors/api/routes/__init__.py
+++ projects/server/src/interactors/api/routes/__init__.py
@@ -5,6 +5,7 @@ from interactors.api.routes.projects import router as projects_router
 from interactors.api.routes.runs import router as runs_router
 from interactors.api.routes.runs import work_items_router as run_start_router
 from interactors.api.routes.teams import agent_definitions_router, teams_router
+from interactors.api.routes.threads import router as threads_router
 from interactors.api.routes.work_items import project_router as project_work_items_router
 from interactors.api.routes.work_items import router as work_items_router
 
@@ -18,3 +19,4 @@ def register_routers(app: FastAPI) -> None:
     app.include_router(runs_router)
     app.include_router(run_start_router)
     app.include_router(notifications_router)
+    app.include_router(threads_router)
